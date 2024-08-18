2a2bfce8496907eb087a159df55bfee52e70f2ae
KONFeature <quentin@nivelais.com>
2024-08-18 17:11:06 +0200

    🚧 A bit of progress with gluegun cli

---
+++ src/cli/commands/generate.ts
@@ -0,0 +1,91 @@
+import { existsSync } from "node:fs";
+import path from "node:path";
+import type { GluegunToolbox } from "gluegun";
+import type { Config } from "../../generatedTypes/erpcTypes";
+import { writeErpcConfig } from "../utils/config";
+
+/**
+ * The command to generate the eRPC config
+ */
+export async function generate({ print, parameters }: GluegunToolbox) {
+    // Header
+    print.info("==============================");
+    print.info("==   eRPC Config Generator  ==");
+    print.info("==============================");
+
+    // Extract the arguments
+    const configFile = parameters.options.config ?? "./erpc-config.ts";
+    const outputFile = parameters.options.out ?? "./erpc.yaml";
+
+    if (typeof configFile !== "string") {
+        throw new Error("Invalid config file");
+    }
+    if (typeof outputFile !== "string") {
+        throw new Error("Invalid output file");
+    }
+
+    // Log a few stuff
+    print.info(" - Parmaeters:");
+    print.info(`   - Configuration file: ${configFile}`);
+    print.info(`   - Output file: ${outputFile}`);
+
+    // Load the user config
+    const config = await loadConfigFromFile(configFile);
+
+    // Start the file writing
+    print.info("Generating the eRPC config file...");
+
+    // Write the erpc config
+    writeErpcConfig({ config, outputPath: outputFile });
+
+    print.success("eRPC config file generated!");
+}
+
+/**
+ * Load the config file
+ * @param configFile
+ * @returns
+ */
+async function loadConfigFromFile(configFile: string): Promise<Config> {
+    // Ensure the config file is a typescript one exist
+    if (!configFile.endsWith(".ts")) {
+        throw new Error("Config file must be a typescript file");
+    }
+
+    const absoluteConfigPath = path.resolve(process.cwd(), configFile);
+
+    // Ensure the config file exist
+    {
+        const fileExist = existsSync(absoluteConfigPath);
+        if (!fileExist) {
+            throw new Error(`Config file not found: ${configFile}`);
+        }
+    }
+
+    // Find the exported config stuff
+    const userConfig = await import(absoluteConfigPath);
+
+    // Check if there's a default export
+    const config = userConfig.default;
+    if (!config) {
+        throw new Error(
+            `No default export found in config file at: ${configFile}`
+        );
+    }
+
+    // Perform a few basics type checks
+    if (typeof config !== "object") {
+        throw new Error("Config must be an object");
+    }
+
+    // Ensure the config has the required fields
+    if (
+        !config.projects ||
+        !config.logLevel ||
+        !Array.isArray(config.projects)
+    ) {
+        throw new Error("Config must have a `projects` and `logLevel` field");
+    }
+
+    return config as Config;
+}
--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,19 +1,21 @@
 import { build } from "gluegun";
+import { generate } from "./commands/generate";
 
 /**
  * Create the cli and kick it off
  */
 const cli = build()
     .brand("erpc-config")
-    .src(__dirname)
-    .plugins("./node_modules", { matching: "erpc-config-*", hidden: true })
-    .plugins("./node_modules", {
-        matching: "@konfeature/erpc-config-generator",
-        hidden: true,
-    })
-    .plugins("./node_modules", { matching: "yaml", hidden: true })
+    //.src(__dirname)
+    .src("/Users/quentin/Workspace/Perso/erpc-config")
     .help() // provides default for help, h, --help, -h
     .version() // provides default for version, v, --version, -v
+    .command({
+        name: "erpc-config",
+        description: "Generate the eRPC config for the current project",
+        alias: ["generate"],
+        run: generate,
+    })
     .create();
 
 /**
